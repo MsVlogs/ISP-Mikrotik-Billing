5e3e70bf8ed43992ac2e404099ab62c20abacc67
MsVlogs <githubbdms@gmail.com>
2026-09-02 02:24:34 +0600

    chore: remove legacy pro branding link

--- resources/views/layouts/partials/userpanel.blade.php
+++ resources/views/layouts/partials/userpanel.blade.php
@@ -135,8 +135,6 @@
 
         <div class="dropdown-menu dropdown-caret dropdown-caret dropdown-menu-end py-0" aria-labelledby="navbarDropdownUser">
             <div class="bg-white dark__bg-1000 rounded-2 py-2">
-                <a class="dropdown-item fw-bold text-warning" href="https://github.com/rohan9222/"><span class="fas fa-crown me-1"></span><span>{{ __('Go Pro') }}</span></a>
-                <div class="dropdown-divider"></div>
                 <a wire:navigate.hover wire:current="active" class="dropdown-item" href="{{route('profile.show')}}">{{ __('Profile & account') }}</a>
                 <div class="dropdown-divider"></div>
                 @if (hasAccess(['Super Admin'], ['site-settings']))
